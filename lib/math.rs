f0ee41145a1785ca5762696d6466c07277a8ab1e
s4l2vx1 <145181636+s4l2vx1@users.noreply.github.com>
2024-03-12 21:49:42 +0900

    update

--- lib/math.rs
+++ lib/math.rs
@@ -3,3 +3,13 @@ fn nck(n: usize, k: usize) -> usize {
     if n - k < k { return nck(n, n - k); }
     (0..=n).rev().zip(1..=k).fold(1, |r, (n, d)| r * n / d)
 }
+
+fn extended_gcd<T: PrimInt>(a: T, b: T) -> (T, T) {
+    if b == T::zero() {
+        (T::one(), T::zero())
+    } else {
+        let (x, y) = extended_gcd(b, a % b);
+        let q = a / b;
+        (y, x - q * y)
+    }
+}
--- lib/shortcut.rs
+++ lib/shortcut.rs
@@ -1,5 +1,5 @@
 //等差数列
-itertools::
+(0..n).iter().step_by(d).take_while(|&x| x < b + d)
 
 //繰り返し適用
 itertools::iterate(a, |&x| x + d).take_while(|&x| x < b + d)

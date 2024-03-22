6e4fae8036bdcf5848698b2e39e1dd1d58d063b2
s4l2vx1 <145181636+s4l2vx1@users.noreply.github.com>
2024-03-22 12:17:14 +0900

    update

--- lib/shortcut.rs
+++ lib/shortcut.rs
@@ -1,8 +1,8 @@
 //等差数列
-(0..n).iter().step_by(d).take_while(|&x| x < b + d)
+(0..n).iter().step_by(d).take_while(|&x| x < b)
 
 //繰り返し適用
-itertools::iterate(a, |&x| x + d).take_while(|&x| x < b + d)
+itertools::iterate(a, |&x| x + d).take_while(|&x| x < b)
 
 //累積和
 a.iter().scan(0, |s, &x| {*s += x; Some(*s)}).collect::<Vec<_>>()
